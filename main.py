842ea67258964592e4ce241f52269ca2ab47dd2e
ronny <ronny@MacBookPro.fritz.box>
2025-08-22 10:32:58 +0200

    auto: Test_Vector: 1 updated, 2 removed @ 2025-08-22 10:32:58

---
+++ main.py
@@ -0,0 +1,12 @@
+# watcher test v2
+
+print("✅ Python funktioniert!")
+
+for i in range(1, 21):
+    print(f"Zahl {i}")
+
+print()
+print("Ich bin Fertig")
+
+name = input("Bitte gib deinen Namen ein: ")
+print(f"{name} 😊")
